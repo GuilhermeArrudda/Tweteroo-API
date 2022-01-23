e9312d91ad2c35efd47701443d542263ebf880c9
Guilherme <guilhermepa3@gmail.com>
2022-01-23 00:50:27 -0300

    get tweets implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,24 @@ app.use(json());
 app.use(cors())
 
 const users = [];
-
 const tweets = [];
+
+function lastTenTweets(n){
+  let list = [];
+
+  for (let i = 0; i < n; i++) {
+    if(i > tweets.length -1){
+      return list
+    }
+    let userTweet = {
+      "username": tweets[tweets.length -1 -i].username,
+      "avatar": users.find(e => e.username === tweets[tweets.length -1 -i].username).avatar,
+      "tweet": tweets[tweets.length -1 -i].tweet
+    }
+    list.push(userTweet)
+  }
+  return list
+}
 
 app.get("/test", (req, res) => {
   res.send(users);
@@ -27,6 +43,10 @@ app.post("/tweets", (req, res) => {
 
   res.send("OK");
 });
+
+app.get("/tweets", (req, res) => {
+  res.send(lastTenTweets(10));
+})
 
 app.listen(5000, () => {
     console.log ('Running app in http://localhost:5000')
